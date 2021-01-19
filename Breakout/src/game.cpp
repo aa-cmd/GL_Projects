65fcf20b1606fe2adf459f573b254cea9484541a
aa-cmd <goethewerther94@gmail.com>
2021-01-19 13:41:46 +0300

    19012021_1341_commit

--- Breakout/src/game.cpp
+++ Breakout/src/game.cpp
@@ -1,17 +1,33 @@
 #include "game.h"
+#include "resource_manager.h"
+#include "sprite_renderer.h"
+
+// Game-related State data
+SpriteRenderer *Renderer;
 
 Game::Game(unsigned int width, unsigned height)
     : State(GAME_ACTIVE), Keys(), Width(width), Height(height)
-    {
-
-    }
-
-Game::~Game()
 {
 
 }
+
+Game::~Game()
+{
+    delete Renderer;
+}
 void Game::Init()
 {
+    //load shaders
+    ResourceManager::LoadShader("./shaders/sprite.vs", "./shaders/sprite.frag", nullptr, "sprite");
+    //configure shaders
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width),
+        static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
+    ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+    ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+    //set render-specific controls
+    Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+    ResourceManager::LoadTexture("./textures/awesomeface.png", true, "face");
+
 
 }
 void Game::Update(float dt)
